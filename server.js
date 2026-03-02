9332de4d5ac397961378c95329075a1044fb724b
Palawi <bapcoloj@gmail.com>
2026-03-02 16:53:16 +0100

    feat(setup): add minimal express tailwind base

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = Number(process.env.PORT) || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "NetflixLight",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
