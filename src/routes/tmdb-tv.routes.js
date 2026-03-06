63288b59fb2be66217928361e25d6de80bde8356
Palawi <bapcoloj@gmail.com>
2026-03-06 16:28:09 +0100

    feat(tmdb): add dedicated popular and top-rated routes

---
+++ src/routes/tmdb-tv.routes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { tmdbGet } = require("../clients/tmdb.client");
+const {
+  parseTmdbPage,
+  parseOptionalTmdbString,
+} = require("./tmdb-query.utils");
+
+const router = express.Router();
+
+router.get("/popular", async (req, res, next) => {
+  try {
+    const page = parseTmdbPage(req.query.page);
+    const language = parseOptionalTmdbString(req.query.language, "language");
+
+    const payload = await tmdbGet("/tv/popular", {
+      query: {
+        page,
+        language,
+      },
+    });
+
+    return res.status(200).json(payload);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get("/top-rated", async (req, res, next) => {
+  try {
+    const page = parseTmdbPage(req.query.page);
+    const language = parseOptionalTmdbString(req.query.language, "language");
+
+    const payload = await tmdbGet("/tv/top_rated", {
+      query: {
+        page,
+        language,
+      },
+    });
+
+    return res.status(200).json(payload);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;
